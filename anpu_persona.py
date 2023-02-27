ba8218251033117dc875c393dfffccc0b9a7ffb0
darkphipps <126334442+darkphipps@users.noreply.github.com>
2023-02-26 18:04:51 -0700

    Adding all files for AI

--- anpu_persona.py
+++ anpu_persona.py
@@ -17,33 +17,14 @@ max_tokens = 256
 # Connect to or create the ontology database
 ontology_storage = OntologyStorage()
 
-import openai
-import os
-import textwrap
-from anpu_storage import OntologyStorage
-from dotenv import load_dotenv
-
-# Load environment variables from a .env file
-load_dotenv()
-
-# Authenticate with OpenAI using the API key
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-
-# Set OpenAI model and parameters
-model_engine = "text-davinci-002"
-max_tokens = 256
-
-# Connect to or create the ontology database
-ontology_storage = OntologyStorage()
-
-def get_anubis_persona():
+def get_anubis_persona(name="Morgan"):
     # Get the current Anubis persona from the ontology database
     anubis_persona = ontology_storage.get_similar_responses("Anubis persona")
 
     # If the persona is not found in the ontology database, generate a new persona using OpenAI
     if anubis_persona is None:
         # Set OpenAI prompt to generate a new Anubis persona
-        prompt = "Generate a new Anubis persona:\nAnubis: "
+        prompt = f"Generate a new Anubis persona for {name}:\nAnubis: "
 
         response = openai.Completion.create(engine=model_engine, prompt=prompt, max_tokens=max_tokens)
 
@@ -56,4 +37,3 @@ def get_anubis_persona():
 
     # Return the Anubis persona
     return anubis_persona
-
